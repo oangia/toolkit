32ec044eeeaaf7ca472e41297af2ad75b8e01988
oangia <hqnhatdn@gmail.com>
2026-09-06 22:00:15 +0700

    Update model.py

--- toolkit/nn/model.py
+++ toolkit/nn/model.py
@@ -7,8 +7,9 @@ class Model:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.generator = generator_class().to(self.device)
     
-    def save_model(self):
-        torch.save(self.generator.state_dict(), self.save_path)
+    def save_model(self, path=None):
+        target_path = path if path is not None else self.save_path
+        torch.save(self.generator.state_dict(), target_path)
 
     def load_model(self):
         if os.path.exists(self.save_path):
