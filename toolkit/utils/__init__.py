20d52b997e75de2d27432e0fe41db4e9ea148d52
oangia <hqnhatdn@gmail.com>
2026-09-09 18:22:45 +0700

    Improve readability by adding a blank line
    Added a blank line for better readability.

--- toolkit/utils/__init__.py
+++ toolkit/utils/__init__.py
@@ -4,7 +4,7 @@ import zipfile
 from torchvision import transforms
 from .image import Image
 from .dataset import BaseImageDataset
-
+ 
 def download(url, folder='./'):
     """Downloads a zip file from a URL and extracts it."""
     zip_name = 'temp_download.zip'
