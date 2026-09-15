8625bc45158c1b4fb679631f26e27bfcff78b38a
oangia <hqnhatdn@gmail.com>
2026-09-15 14:36:50 +0700

    Refactor input assignment for clarity

--- toolkit/utils/data/enhance.py
+++ toolkit/utils/data/enhance.py
@@ -27,7 +27,7 @@ class ImageEnhanceDataset(BaseImageDataset):
         chunks = img_obj.slice_image(self.input_size)
         
         target = random.choice(chunks)
-        input = self._low(UImage(target).lower_quality().toPIL())
+        input = UImage(target).lower_quality().toPIL()
 
         t_tgt = self.transform(target)
         t_inp = self.transform(input)
