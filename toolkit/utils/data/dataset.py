b5971dd68bfda1ac8ff07868ca99290956cd0553
oangia <hqnhatdn@gmail.com>
2026-09-15 14:05:50 +0700

    Update dataset.py

--- toolkit/utils/data/dataset.py
+++ toolkit/utils/data/dataset.py
@@ -3,7 +3,7 @@ import random
 import torch
 import cv2
 import numpy as np
-from toolkit.utils import Image
+from toolkit.utils import UImage
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 import torchvision.transforms.functional as TF
